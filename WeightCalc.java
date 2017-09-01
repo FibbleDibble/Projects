07153b91036045879dfe2fbde97d1a5c7d2e6db0
FibbleDibble <527518@my.sbunified.org>
2017-09-01 11:49:01 -0700

    WeightCalc

---
+++ WeightCalc.java
@@ -0,0 +1,14 @@
+public class WeightCalc
+{
+	public static void main (String[] args)
+	{
+		int x = Integer.parseInt (args[0]);
+		
+		System.out.println("Mars:"+x*0.38);
+		System.out.println("Jupiter:"+x*2.36);
+		System.out.println("Venus:"+x*0.91);
+		System.out.println("Saturn:"+x*1.09);
+		
+		double y = Double.parseDouble (args[0]);
+	}
+}
